292bc8a6db97c53f4afaf16ec0246c2d995ecc4f
startprogress <haobo_nice@163.com>
2018-09-01 16:18:37 +0800

    Update function.jl

--- script/function.jl
+++ script/function.jl
@@ -10,6 +10,13 @@ g = f
 
 println(g(2, 3))
 
+# convert return type
+function l(x, y)::Int8
+  return x * y
+end;
+
+println(typeof(l(2,3)))
+
 # multiple Return Values
 function h(x,y)
   return x+y, x*y
@@ -24,6 +31,18 @@ println(k(1,2,3,4,5))
 # anonymous functions
 println(map(x -> x^2 + 2x - 1, [1,3,-1]))
 
-# hello world
-using Main.mymodule
-mymodule.helloworld()
+# optional arguments
+function m(x, y = 1, z = 3)
+  return x + y + z
+end
+println(m(1))
+println(m(1, 3, 5))
+
+# keyword arguments can make these complex interfaces easier to use and extend by 
+# allowing arguments to be identified by name instead of only by position
+# keyword arguments are defined using a semicolon in the signature
+function plot(x, y; style="solid", width=1, color="black")
+  return x + y + 1, style * color
+end
+println(plot(1, 2, style = "soft"))
+println(plot(2, 3, color = "blue", style = "soft"))
